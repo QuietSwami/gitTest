e8a5acadbd662371f0cbe7b3dc670280be667baa
QuietSwami <fran.abm94@gmail.com>
2016-01-21 21:57:55 +0000

    First git commit

---
+++ test.py
@@ -0,0 +1,14 @@
+class MyClass:
+	att = 5555;
+
+	def hello (self):
+		print "Hello"
+	def changeAtt (self, numb):
+		self.att = numb
+
+inte = MyClass()
+inte.hello()
+inte.changeAtt(55)
+print inte.att
+inte.att = 555
+print inte.att
