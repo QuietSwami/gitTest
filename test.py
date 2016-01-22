698bb4aa9d0084d15171a1ef1c184936377ee65c
QuietSwami <fran.abm94@gmail.com>
2016-01-22 13:39:59 +0000

    cherry.expose exposes the function to the server

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class StringGenerator(object):
 	@cherrypy.expose
 	def index(self):
 		return "<h1>Hello World!</h1> <h2>Welcome</h2>"
-	
+	@cherrypy.expose
 	def generate(self):
 		return ''.join(random.sample(string.hexadigits, 8))
 
